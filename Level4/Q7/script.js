0b6a4de90df7c116a390d3777e0243b523952f5b
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-26 21:35:45 +0500

    Changes made

--- Level4/Q7/script.js
+++ Level4/Q7/script.js
@@ -1,7 +1,9 @@
 var arr = [1, 2, 3, 4];
-var first = arr[0];
 
-for (var i = 0; i < arr.length - 1; i++) {
+var    first = arr[0 ];
+
+for  (var i = 0; i < arr.length - 1; i++) {
+
     arr[i] = arr[i + 1];
 }
 arr[arr.length - 1] = first;
