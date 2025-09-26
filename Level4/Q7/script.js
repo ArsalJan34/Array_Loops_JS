7843f938a4faf7f63745057789d62e6067d6b529
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-26 21:31:39 +0500

    Changes made

---
+++ Level4/Q7/script.js
@@ -0,0 +1,9 @@
+var arr = [1, 2, 3, 4];
+var first = arr[0];
+
+for (var i = 0; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+}
+arr[arr.length - 1] = first;
+
+document.write("Array after left rotation: ", arr);
