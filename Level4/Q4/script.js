8fbea63edc43c343c4948941ee9fa48c5142d845
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-26 21:29:30 +0500

    Changes made

---
+++ Level4/Q4/script.js
@@ -0,0 +1,13 @@
+var arr = [5, 8, 12, 3, 9];
+var smallest = arr[0];
+var secondSmallest = arr[0];
+
+for (var i = 1; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+        secondSmallest = smallest;
+        smallest = arr[i];
+    } else if (arr[i] < secondSmallest && arr[i] !== smallest) {
+        secondSmallest = arr[i];
+    }
+}
+console.log("Second Smallest =", secondSmallest);
