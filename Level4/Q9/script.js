3ab0023830e1c440459e709b963199b10cdd6131
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-26 21:32:59 +0500

    Changes made

---
+++ Level4/Q9/script.js
@@ -0,0 +1,14 @@
+var arr = [1, 2, 3, 4, 5];
+var sorted = true;
+
+for (var i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+        sorted = false;
+        break;
+    }
+}
+if (sorted) {
+    document.write("Array is sorted");
+} else {
+    document.write("Array is not sorted");
+}
