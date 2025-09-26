a75a6a66dfb586cde33b7c0b4cb51730258d19c3
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-26 21:36:03 +0500

    Changes made

--- Level4/Q3/script.js
+++ Level4/Q3/script.js
@@ -2,12 +2,15 @@ var arr = [5, 8, 12, 3, 9];
 var largest = arr[0];
 var secondLargest = arr[0];
 
-for (var i = 1; i < arr.length; i++) {
-    if (arr[i] > largest) {
+for (var i = 1; i < arr.length; i++) 
+    {
+    if (arr[i] > largest) 
+        {
         secondLargest = largest;
         largest = arr[i];
-    } else if (arr[i] > secondLargest && arr[i] !== largest) {
+    } 
+    else if (arr[i] > secondLargest && arr[i] !== largest) {
         secondLargest = arr[i];
-    }
+                      }
 }
 document.write("Second Largest = ", secondLargest);
