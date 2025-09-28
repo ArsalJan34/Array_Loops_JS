4957732e9c8a17b7963d9c03c9f0dd2caf1b41f7
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-28 13:46:08 +0500

    Changes made

--- Splice_slice/Example2/script.js
+++ Splice_slice/Example2/script.js
@@ -1,16 +1,15 @@
-let languages = ["JavaScript", "Python", "C++", "Java", "Go"];
+var languages = ["JavaScript", "Python", "C++", "Java", "Go"];
 
 var remove1 = languages.splice(2, 1);
 
-document.write("Removed:", removed1); 
+document.write("Removed : ", remove1); 
 
-
-document.write("Modified:", languages);  
+document.write("<br>Modified : ", languages);  
 
 languages.splice(2, 0, "C#", "Ruby");
 
-document.write("After adding:", languages);  
+document.write("<br>After adding : ", languages);  
 
 languages.splice(1, 2, "PHP", "TypeScript");
 
-document.write("After replacing:", languages);  
+document.write("<br>After replacing : ", languages);  
